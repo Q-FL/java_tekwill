56c9e24b694333f0f6129209b88b6a9d287368e4
Florin Fl <florentinflhd@gmail.com>
2019-12-06 18:42:38 +0200

    asd

--- L5/EX5_19_1.java
+++ L5/EX5_19_1.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class EX5_19_1 {
 	public static void main(String[] Strings) {
-		int count = 0;
-		int sum = 1;
-		while (count < 101) {
+		int count = 0,sum = 1;
+		while (count <= 101) {
 			sum += count;
 			count++;
 			System.out.print("Count: " + count + " | Sum: " + sum + "\n");
