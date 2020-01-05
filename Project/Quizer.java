9981bed180fe99714230e3d4093a066ed53c1cfa
Florin Fl <florentinflhd@gmail.com>
2020-01-05 21:50:34 +0200

    Added QuestionClass as database for questions, and added some logic for Quizer init.

--- Project/Quizer.java
+++ Project/Quizer.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Quizer {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		UserDataBase userDB = new UserDataBase();
+		Random rand = new Random();
 
-		userDB.getAllUsersInfo();
+		QuestionClass questionDataBase = new QuestionClass();
+		boolean flag = true;
+		while (flag) {
+			int randomInt = rand.nextInt(5) + 1;
+			String[] qData = questionDataBase.questionSelector(randomInt);
+			boolean flag2 = true;
+			while (flag2) {			
+				System.out.println(qData[0]+"\n"+qData[1]);
 
-		System.out.print("New User Name: ");
-		String name = input.nextLine();
-		userDB.addNewUser(name);
-		userDB.getAllUsersInfo();
-
-		// System.out.print("New User Score: ");
-		// int score = input.nextInt();
-		// userDB.updateUserScore(name,score);
-		// userDB.getAllUsersInfo();
-
-		// System.out.print("Remove User Name: ");
-		// name = input.nextLine();
-		// userDB.removeUser(name);
-		// userDB.getAllUsersInfo();
-
-		// System.out.print("Info User Name: ");
-		// name = input.nextLine();
-		// userDB.getUserInfo(name);
+				System.out.print("Chose the right answer: ");
+				String userChoice = input.nextLine();
+				userChoice = userChoice.toUpperCase();
+				
+				if (userChoice.equals(qData[2])) {
+					System.out.println("YAY!");
+					break;
+				} else {
+					switch (userChoice) {
+						default : System.out.println("Wrong Input"); break;
+						case "FIFTY" : System.out.println("50/50"); break;
+						case "PHONE" : System.out.println("Call a Friend"); break;
+						case "ASK" : System.out.println("Ask the Audience"); break;
+						case "EXIT" : flag = false; flag2 = false; break;
+					}
+				}
+			}
+		}
 	}
 }
